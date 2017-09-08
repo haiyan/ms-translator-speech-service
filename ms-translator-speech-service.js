d7d70777ebe9781fd3e19097c7658009343d9237
Haiyan Zhang <haiyan@gmail.com>
2017-09-08 23:49:28 +0100

    Allow file stream to resume if connection is reset.
    Allow file audiostream to pause and resume if connection is reset. audioStream object now exists separately from connection object, pauses and resumes if the connection is unexpectedly closed.

--- ms-translator-speech-service.js
+++ ms-translator-speech-service.js
@@ -19,71 +19,81 @@ const translatorService = function init(options) {
   };
 
   Object.assign(this, defaultOptions, options);
-  
+  this.sendFile = sendFile;
+
   const featureStrings = Object.keys(this.features).filter((key) => {
     return !!this.features[key];
   });
 
   const featureQueryString = featureStrings.length ? `&features=${featureStrings.join(',').toLowerCase()}` : '';
   const voiceQueryString = this.voice.length ? `&voice=${this.voice}` : '';
-  const formatQueryString = this.format.length? `&format=${this.format}` : '';
-  
+  const formatQueryString = this.format.length ? `&format=${this.format}` : '';
+
   const speechTranslateShortUrl = 'wss://dev.microsofttranslator.com/speech/translate';
   this.speechTranslateUrl = `${speechTranslateShortUrl}?api-version=${this.apiVersion}&from=${this.fromLanguage}&to=${this.toLanguage}${featureQueryString}&ProfanityMarker=${this.profanityMarker}&ProfanityAction=${this.profanityAction}${voiceQueryString}${formatQueryString}`;
-  
+
   this.issueTokenUrl = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken';
 };
 
-translatorService.prototype._requestAccessToken = function(callback) {
+translatorService.prototype._requestAccessToken = function (callback) {
   const postRequest = {
     url: this.issueTokenUrl,
     headers: {
-     'Ocp-Apim-Subscription-Key': this.subscriptionKey 
+      'Ocp-Apim-Subscription-Key': this.subscriptionKey
     }
   };
- 
+
   debug('requesting access token for translation endpoint');
-   
+
   // request token
   request.post(postRequest, (error, response, body) => {
     return callback(error, body);
-  }); 
+  });
 };
 
-translatorService.prototype._connectToWebsocket = function(accessToken, callback) {
+translatorService.prototype._connectToWebsocket = function (accessToken, callback) {
   // create new client
   const ws = new wsClient();
-      
+
   // event for connection failure
   ws.once('connectFailed', (error) => {
     debug('connection to translation endpoint failed:', error);
 
     return callback(error);
   });
-              
+
   // event for connection success
   ws.once('connect', (connection) => {
     debug('connection to translation endpoint succeeded');
 
     this.connection = connection;
-    
-    this.connection.sendFile = sendFile;
+
+    this.connection.on('close', () => {
+      if (this.audioStream) {
+        debug('connection closed unexpectedly');
+        this.audioStream.pause();
+        this.start((err, service) => {
+          debug('reset connection, resuming file stream');
+          this.audioStream.resume();
+        });
+      }
+    })
 
     // return the successful socket connection for use 
-    return callback(null, connection);	
+    return callback(null, connection);
   });
- 
+
   debug('connecting to translation endpoint');
-   
-   const wsheaders = {
-     'X-ClientTraceId': this.clientTraceId,
-     'Authorization': `Bearer ${accessToken}`
-   };
+
+  const wsheaders = {
+    'X-ClientTraceId': this.clientTraceId,
+    'Authorization': `Bearer ${accessToken}`
+  };
   // connect to the service
   ws.connect(this.speechTranslateUrl, null, null, wsheaders);
 };
 
-translatorService.prototype.start = function(callback) {
+translatorService.prototype.start = function (callback) {
   if (this.subscriptionKey === undefined) {
     const error = new Error('Missing subscriptionKey option - please find your key via the Azure portal.');
     return callback(error);
@@ -93,19 +103,20 @@ translatorService.prototype.start = function(callback) {
     if (error) return callback(error);
     debug('acquired access token for translation endpoint');
 
-    this._connectToWebsocket(accessToken, callback); 
+    this._connectToWebsocket(accessToken, callback);
   });
 };
 
-translatorService.prototype.stop = function(callback) {
+translatorService.prototype.stop = function (callback) {
   if (!this.connection) return callback(null);
+  this.audioStream = null;
   this.connection.once('close', callback);
   this.connection.close();
   debug('closed translation endpoint connection');
 };
 
 // WIP
-translatorService.supportedLanguages = function() {
+translatorService.supportedLanguages = function () {
   return '';
 };
 
